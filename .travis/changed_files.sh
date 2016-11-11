1f97d45f1eda8fe68e683baa2feacd3929fad85e
Rasmus Johansson <rasmus@mariadb.com>
2016-11-11 10:15:57 +0200

    added script for showing changed files

---
+++ .travis/changed_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# File:   changed_files.sh
+# Author: rasmus
+#
+# Created on Nov 11, 2016, 10:12:22 AM
+#
+
+echo TRAVIS_COMMIT_RANGE: $TRAVIS_COMMIT_RANGE
+CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
+echo CHANGED_FILES
